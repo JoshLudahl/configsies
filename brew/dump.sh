60e169af6d9bde0bf29bbdb62527b4c6078501c9
Josh Ludahl <josh.ludahl@dat.com>
2020-09-30 12:49:39 -0700

    simlified

--- brew/dump.sh
+++ brew/dump.sh
@@ -1,3 +1,75 @@
-#!/bin/bash
-brew leaves > leaves
-brew cask list > casks
+#!/bin//env bash
+# MacOS setup
+
+echo "Starting to install the applications"
+
+xcode-select --install
+
+# Homebrew leaves
+LEAVES=(
+    automake
+    aws-iam-authenticator
+    binutils
+    bundletool
+    capstone
+    coreutils
+    coq
+    elixir
+    exiftool
+    gcc
+    graphviz
+    helm
+    isl
+    jq
+    john-jumbo
+    ktlint
+    kubectx
+    lean
+    libtool
+    md5sha1sum
+    mit-scheme
+    node
+    openvpn
+    postgresql
+    rlwrap
+    telnet
+    tmux
+    vim
+    wget
+    zlib
+)
+
+echo "Installing Leaves..."
+brew install ${LEAVES[@]}
+
+brew link --force readline
+
+# Homebrew casks
+CASKS=(
+    adoptopenjdk
+    burp-suite
+    eloston-chromium
+    firefox
+    flux
+    intellij-idea-ce
+    obs
+    qbittorrent
+    rectangle
+    scroll-reverser
+    sensiblesidebuttons
+    slack
+    spotify
+    vagrant
+    virtualbox
+    visual-studio-code
+    vlc
+    wireshark
+    xquartz
+    zotero
+)
+
+echo "Installing cask apps..."
+brew cask install ${CASKS[@]}
+
+
+echo "Macbook setup completed!"
